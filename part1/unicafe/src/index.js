697297a54cca68db2dc3b3cbba643b6d688e343c
marianopedrini <pedrini.mariano96@gmail.com>
2021-11-26 01:19:13 -0300

    1.11

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -5,35 +5,50 @@ const Button = ({ handleClick, text }) => {
   return <button onClick={handleClick}>{text}</button>;
 };
 
-const Display = ({ good, neutral, bad }) => {
+const Statistic = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
+
+const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad;
   const average = (good - bad) / total;
   const positive = (good * 100) / total;
   return (
-    <div>
-      <p>Good: {good} </p>
-      <p>Neutral: {neutral} </p>
-      <p>Bad: {bad} </p>
-      <p>Total: {total} </p>
-      <p>Average: {average} </p>
-      <p>Positive: {isNaN(positive) ? 0 : positive} </p>
-    </div>
+    <table>
+      <tbody>
+        <Statistic text="Good:" value={good} />
+        <Statistic text="Neutral:" value={neutral} />
+        <Statistic text="Bad:" value={bad} />
+        <Statistic text="Total:" value={total} />
+        <Statistic text="Average:" value={average} />
+        <Statistic text="Positive:" value={isNaN(positive) ? 0 : positive} />
+      </tbody>
+    </table>
   );
 };
 
 const App = () => {
+  const [isFeedback, setIsFeedback] = useState(false);
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
   const rateGood = () => {
     setGood(good + 1);
+    setIsFeedback(true);
   };
   const rateNeutral = () => {
     setNeutral(neutral + 1);
+    setIsFeedback(true);
   };
   const rateBad = () => {
     setBad(bad + 1);
+    setIsFeedback(true);
   };
 
   return (
@@ -44,7 +59,11 @@ const App = () => {
       <Button handleClick={rateBad} text="Bad" />
 
       <h1>Statistics </h1>
-      <Display good={good} neutral={neutral} bad={bad} />
+      {!isFeedback ? (
+        'No Feedback given'
+      ) : (
+        <Statistics good={good} neutral={neutral} bad={bad} />
+      )}
     </div>
   );
 };
